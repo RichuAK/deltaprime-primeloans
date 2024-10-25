dded2d9e116cac0a759050ff1db1eeeff267252c
kavsky02 <kamilovsky@deltaprime.io>
2024-10-25 18:17:07 +0200

    WIP: RtknToPrimeConverter

---
+++ contracts/token/RtknToPrimeConverter.sol
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract RtknToPrimeConverter is Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
+    IERC20 public rTKN;
+
+    uint256 public constant CONVERSION_RATIO = 1e18; // Scaled by 1e18 for precision
+    uint256 public rRTKNMaxCap;
+
+    enum Phase { Phase1, Phase2 }
+    Phase public currentPhase;
+
+    mapping(address => uint256) public userrTKNPledged;
+    mapping(address => bool) public userProcessed;
+    address[] public users;
+
+    uint256 public totalrTKNPledged;
+    uint256 public totalAdjustedrTKNPledged;
+    uint256 public scalingFactor; // Scaled by 1e18 for precision
+    uint256 public currentBatchIndex;
+
+    // Event definitions
+    event Pledged(address indexed user, uint256 amount);
+    event PhaseStarted(Phase newPhase, uint256 scalingFactor);
+    event UserProcessed(address indexed user, uint256 adjustedPledgedAmount, uint256 excessAmount);
+    event Withdrawal(address indexed owner, uint256 amount);
+    event MaxCapSet(uint256 prevMaxCap, uint256 newMaxCap);
+
+    constructor(
+        address _rTKNAddress,
+        uint256 _rRTKNMaxCap
+    ) {
+        require(_rTKNAddress != address(0), "Invalid token address");
+        require(_rRTKNMaxCap > 0, "Max cap must be greater than zero");
+
+        rTKN = IERC20(_rTKNAddress);
+        rRTKNMaxCap = _rRTKNMaxCap;
+        currentPhase = Phase.Phase1;
+    }
+
+    function setRTKNMaxCap(uint256 _newMaxCap) external onlyOwner {
+        require(currentPhase == Phase.Phase1 && totalrTKNPledged == 0, "Cannot change cap after pledging has started");
+        emit MaxCapSet(rRTKNMaxCap, _newMaxCap);
+        rRTKNMaxCap = _newMaxCap;
+    }
+
+    function pledgerTKN(uint256 amount) external nonReentrant {
+        require(currentPhase == Phase.Phase1, "Pledging not allowed in current phase");
+        require(amount > 0, "Amount must be greater than zero");
+
+        rTKN.safeTransferFrom(msg.sender, address(this), amount);
+
+        if (userrTKNPledged[msg.sender] == 0) {
+            users.push(msg.sender);
+        }
+        userrTKNPledged[msg.sender] += amount;
+        totalrTKNPledged += amount;
+
+        emit Pledged(msg.sender, amount);
+    }
+
+    function startPhase2() external onlyOwner {
+        require(currentPhase == Phase.Phase1, "Already in Phase 2");
+        currentPhase = Phase.Phase2;
+
+        uint256 totalrRTKNDemanded = (totalrTKNPledged * CONVERSION_RATIO) / 1e18;
+        if (totalrRTKNDemanded <= rRTKNMaxCap) {
+            scalingFactor = 1e18;
+        } else {
+            scalingFactor = (rRTKNMaxCap * 1e18) / totalrRTKNDemanded;
+        }
+
+        emit PhaseStarted(currentPhase, scalingFactor);
+    }
+
+    function processUsers(uint256 batchSize) external nonReentrant {
+        require(currentPhase == Phase.Phase2, "Must be in Phase 2");
+
+        uint256 totalUsers = users.length;
+        uint256 endIndex = currentBatchIndex + batchSize;
+        if (endIndex > totalUsers) {
+            endIndex = totalUsers;
+        }
+
+        for (uint256 i = currentBatchIndex; i < endIndex; i++) {
+            address user = users[i];
+            if (!userProcessed[user]) {
+                uint256 pledgedAmount = userrTKNPledged[user];
+                uint256 adjustedPledgedAmount = (pledgedAmount * scalingFactor) / 1e18;
+                uint256 excessAmount = pledgedAmount - adjustedPledgedAmount;
+
+                if (excessAmount > 0) {
+                    rTKN.safeTransfer(user, excessAmount);
+                }
+
+                userrTKNPledged[user] = adjustedPledgedAmount;
+                totalAdjustedrTKNPledged += adjustedPledgedAmount;
+                userProcessed[user] = true;
+
+                emit UserProcessed(user, adjustedPledgedAmount, excessAmount);
+            }
+        }
+
+        currentBatchIndex = endIndex;
+    }
+
+    function withdrawrTKN() external onlyOwner nonReentrant {
+        require(currentPhase == Phase.Phase2, "Must be in Phase 2");
+        require(currentBatchIndex == users.length, "Processing not complete");
+
+        uint256 contractBalance = rTKN.balanceOf(address(this));
+        rTKN.safeTransfer(owner(), contractBalance);
+
+        emit Withdrawal(owner(), contractBalance);
+    }
+
+    function getTotalUsers() external view returns (uint256) {
+        return users.length;
+    }
+}
---
+++ test/local/rtkn-to-prime-converter.test.ts
@@ -0,0 +1,284 @@
+import {fromWei} from "../_helpers";
+
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("RtknToPrimeConverter Contract", function () {
+    let owner, addr1, addr2, addr3, addr4, addrs;
+    let rtknToPrimeConverter;
+    let rtkn;
+    const rRTKNMaxCap = ethers.utils.parseEther("1000"); // Max cap in PRIME tokens
+
+    beforeEach(async function () {
+        // Get signers
+        [owner, addr1, addr2, addr3, addr4, ...addrs] = await ethers.getSigners();
+
+        // Deploy a mock ERC20 token to act as rTKN
+        const RTKNFactory = await ethers.getContractFactory("ERC20Mock");
+        rtkn = await RTKNFactory.deploy("Reward Token", "rTKN", owner.address, ethers.utils.parseEther("100000"));
+        await rtkn.deployed();
+
+        // Deploy the RtknToPrimeConverter contract
+        const RtknToPrimeConverterFactory = await ethers.getContractFactory("RtknToPrimeConverter");
+        rtknToPrimeConverter = await RtknToPrimeConverterFactory.deploy(
+            rtkn.address,
+            rRTKNMaxCap
+        );
+        await rtknToPrimeConverter.deployed();
+
+        // Retrieve the CONVERSION_RATIO from the contract
+        const CONVERSION_RATIO = await rtknToPrimeConverter.CONVERSION_RATIO();
+
+        // Distribute rTKN to test accounts
+        const distributeAmount = ethers.utils.parseEther("20000");
+        await rtkn.transfer(addr1.address, distributeAmount);
+        await rtkn.transfer(addr2.address, distributeAmount);
+        await rtkn.transfer(addr3.address, distributeAmount);
+        await rtkn.transfer(addr4.address, distributeAmount);
+    });
+
+    describe("Deployment", function () {
+        it("Should set the correct initial values", async function () {
+            expect(await rtknToPrimeConverter.rTKN()).to.equal(rtkn.address);
+            expect(await rtknToPrimeConverter.rRTKNMaxCap()).to.equal(rRTKNMaxCap);
+            expect(await rtknToPrimeConverter.currentPhase()).to.equal(0); // Phase1
+
+            // Check that CONVERSION_RATIO is set correctly (assuming it's set within the contract)
+            const CONVERSION_RATIO = await rtknToPrimeConverter.CONVERSION_RATIO();
+            expect(ethers.BigNumber.isBigNumber(CONVERSION_RATIO)).to.be.true;
+        });
+    });
+
+    describe("Phase 1: Pledging", function () {
+        it("Should allow users to pledge rTKN", async function () {
+            const pledgeAmount = ethers.utils.parseEther("100");
+            await rtkn.connect(addr1).approve(rtknToPrimeConverter.address, pledgeAmount);
+
+            await expect(rtknToPrimeConverter.connect(addr1).pledgerTKN(pledgeAmount))
+                .to.emit(rtknToPrimeConverter, "Pledged")
+                .withArgs(addr1.address, pledgeAmount);
+
+            expect(await rtknToPrimeConverter.userrTKNPledged(addr1.address)).to.equal(pledgeAmount);
+            expect(await rtknToPrimeConverter.totalrTKNPledged()).to.equal(pledgeAmount);
+        });
+
+        it("Should not allow pledging zero amount", async function () {
+            await expect(rtknToPrimeConverter.connect(addr1).pledgerTKN(0)).to.be.revertedWith("Amount must be greater than zero");
+        });
+
+        it("Should not allow pledging without approval", async function () {
+            const pledgeAmount = ethers.utils.parseEther("100");
+            await expect(rtknToPrimeConverter.connect(addr1).pledgerTKN(pledgeAmount)).to.be.reverted;
+        });
+
+        it("Should accumulate pledges from multiple users", async function () {
+            const pledgeAmount = ethers.utils.parseEther("100");
+            await rtkn.connect(addr1).approve(rtknToPrimeConverter.address, pledgeAmount);
+            await rtkn.connect(addr2).approve(rtknToPrimeConverter.address, pledgeAmount);
+
+            await rtknToPrimeConverter.connect(addr1).pledgerTKN(pledgeAmount);
+            await rtknToPrimeConverter.connect(addr2).pledgerTKN(pledgeAmount);
+
+            expect(await rtknToPrimeConverter.totalrTKNPledged()).to.equal(ethers.utils.parseEther("200"));
+        });
+
+        it("Should not allow pledging in Phase 2", async function () {
+            await rtknToPrimeConverter.connect(owner).startPhase2();
+            const pledgeAmount = ethers.utils.parseEther("100");
+            await expect(rtknToPrimeConverter.connect(addr1).pledgerTKN(pledgeAmount)).to.be.revertedWith("Pledging not allowed in current phase");
+        });
+    });
+
+    describe("Phase Transition", function () {
+        beforeEach(async function () {
+            const pledgeAmount = ethers.utils.parseEther("1000");
+            await rtkn.connect(addr1).approve(rtknToPrimeConverter.address, pledgeAmount);
+            await rtknToPrimeConverter.connect(addr1).pledgerTKN(pledgeAmount);
+        });
+
+        it("Should only allow owner to start Phase 2", async function () {
+            await expect(rtknToPrimeConverter.connect(addr1).startPhase2()).to.be.revertedWith("Ownable: caller is not the owner");
+        });
+
+        it("Should transition to Phase 2 correctly", async function () {
+            await rtknToPrimeConverter.connect(owner).startPhase2();
+            expect(await rtknToPrimeConverter.currentPhase()).to.equal(1); // Phase2
+        });
+
+        it("Should calculate scaling factor correctly when demand is within cap", async function () {
+            await rtknToPrimeConverter.connect(owner).startPhase2();
+            expect(await rtknToPrimeConverter.scalingFactor()).to.equal(ethers.utils.parseEther("1"));
+        });
+
+        it("Should calculate scaling factor correctly when demand exceeds cap", async function () {
+            const extraPledgeAmount = ethers.utils.parseEther("5000");
+            await rtkn.connect(addr2).approve(rtknToPrimeConverter.address, extraPledgeAmount);
+            await rtknToPrimeConverter.connect(addr2).pledgerTKN(extraPledgeAmount);
+
+            await rtknToPrimeConverter.connect(owner).startPhase2();
+
+            const CONVERSION_RATIO = fromWei(await rtknToPrimeConverter.CONVERSION_RATIO());
+            const totalDemand = fromWei(await rtknToPrimeConverter.totalrTKNPledged()) * CONVERSION_RATIO;
+
+            let rTKNMaxCapNormalized = fromWei(rRTKNMaxCap)
+            const expectedScalingFactor = rTKNMaxCapNormalized /totalDemand;
+
+            expect(fromWei(await rtknToPrimeConverter.scalingFactor())).to.equal(expectedScalingFactor);
+        });
+    });
+
+    describe("Phase 2: Processing Users", function () {
+        it("Should process users in batches and adjust pledged amounts", async function () {
+            const pledgeAmount1 = ethers.utils.parseEther("1000");
+            const pledgeAmount2 = ethers.utils.parseEther("500");
+            const pledgeAmount3 = ethers.utils.parseEther("2000");
+
+            await rtkn.connect(addr1).approve(rtknToPrimeConverter.address, pledgeAmount1);
+            await rtkn.connect(addr2).approve(rtknToPrimeConverter.address, pledgeAmount2);
+            await rtkn.connect(addr3).approve(rtknToPrimeConverter.address, pledgeAmount3);
+
+            await rtknToPrimeConverter.connect(addr1).pledgerTKN(pledgeAmount1);
+
+            await rtknToPrimeConverter.connect(addr2).pledgerTKN(pledgeAmount2);
+            await rtknToPrimeConverter.connect(addr3).pledgerTKN(pledgeAmount3);
+
+            await rtknToPrimeConverter.connect(owner).startPhase2();
+
+            const batchSize = 2;
+            await rtknToPrimeConverter.connect(addr1).processUsers(batchSize);
+
+            expect(await rtknToPrimeConverter.userProcessed(addr1.address)).to.be.true;
+            expect(await rtknToPrimeConverter.userProcessed(addr2.address)).to.be.true;
+            expect(await rtknToPrimeConverter.userProcessed(addr3.address)).to.be.false;
+
+            // Process remaining users
+            await rtknToPrimeConverter.connect(addr1).processUsers(batchSize);
+            expect(await rtknToPrimeConverter.userProcessed(addr3.address)).to.be.true;
+        });
+
+        it("Should refund excess rTKN when demand exceeds cap", async function () {
+            const pledgeAmount1 = ethers.utils.parseEther("1000");
+            const pledgeAmount2 = ethers.utils.parseEther("500");
+            const pledgeAmount3 = ethers.utils.parseEther("2000");
+
+            await rtkn.connect(addr1).approve(rtknToPrimeConverter.address, pledgeAmount1);
+            await rtkn.connect(addr2).approve(rtknToPrimeConverter.address, pledgeAmount2);
+            await rtkn.connect(addr3).approve(rtknToPrimeConverter.address, pledgeAmount3);
+
+            await rtknToPrimeConverter.connect(addr1).pledgerTKN(pledgeAmount1);
+
+            await rtknToPrimeConverter.connect(addr2).pledgerTKN(pledgeAmount2);
+            await rtknToPrimeConverter.connect(addr3).pledgerTKN(pledgeAmount3);
+
+            await rtknToPrimeConverter.connect(owner).startPhase2();
+
+            const CONVERSION_RATIO = await rtknToPrimeConverter.CONVERSION_RATIO();
+            const totalDemand = (await rtknToPrimeConverter.totalrTKNPledged()).mul(CONVERSION_RATIO);
+            const scalingFactor = await rtknToPrimeConverter.scalingFactor();
+
+            // If scaling factor is less than 1, there should be refunds
+            if (scalingFactor.lt(ethers.utils.parseEther("1"))) {
+                const addr1InitialBalance = await rtkn.balanceOf(addr1.address);
+
+                const batchSize = 3;
+                await rtknToPrimeConverter.connect(addr1).processUsers(batchSize);
+
+                const pledgedAmount = ethers.utils.parseEther("1000");
+                const adjustedPledged = pledgedAmount.mul(scalingFactor).div(ethers.utils.parseEther("1"));
+                const excess = pledgedAmount.sub(adjustedPledged);
+
+                const addr1FinalBalance = await rtkn.balanceOf(addr1.address);
+                expect(addr1FinalBalance).to.equal(addr1InitialBalance.add(excess));
+            }
+        });
+
+        it("Should not refund when scaling factor is 1", async function () {
+            const pledgeAmount1 = ethers.utils.parseEther("100");
+            const pledgeAmount2 = ethers.utils.parseEther("500");
+            const pledgeAmount3 = ethers.utils.parseEther("200");
+
+            await rtkn.connect(addr1).approve(rtknToPrimeConverter.address, pledgeAmount1);
+            await rtkn.connect(addr2).approve(rtknToPrimeConverter.address, pledgeAmount2);
+            await rtkn.connect(addr3).approve(rtknToPrimeConverter.address, pledgeAmount3);
+
+            await rtknToPrimeConverter.connect(addr1).pledgerTKN(pledgeAmount1);
+
+            await rtknToPrimeConverter.connect(addr2).pledgerTKN(pledgeAmount2);
+            await rtknToPrimeConverter.connect(addr3).pledgerTKN(pledgeAmount3);
+
+            await expect(rtknToPrimeConverter.connect(addr1).processUsers(1)).to.be.revertedWith("Must be in Phase 2");
+            await rtknToPrimeConverter.connect(owner).startPhase2();
+
+            const addr1InitialBalance = await rtkn.balanceOf(addr1.address);
+
+            const batchSize = 3;
+            await rtknToPrimeConverter.connect(addr1).processUsers(batchSize);
+
+            const addr1Balance = await rtkn.balanceOf(addr1.address);
+            expect(addr1Balance).to.equal(addr1InitialBalance);
+        });
+    });
+
+    describe("Withdrawal", function () {
+        beforeEach(async function () {
+            const pledgeAmount1 = ethers.utils.parseEther("1000");
+            const pledgeAmount2 = ethers.utils.parseEther("500");
+            const pledgeAmount3 = ethers.utils.parseEther("2000");
+
+            await rtkn.connect(addr1).approve(rtknToPrimeConverter.address, pledgeAmount1);
+            await rtkn.connect(addr2).approve(rtknToPrimeConverter.address, pledgeAmount2);
+            await rtkn.connect(addr3).approve(rtknToPrimeConverter.address, pledgeAmount3);
+
+            await rtknToPrimeConverter.connect(addr1).pledgerTKN(pledgeAmount1);
+            await rtknToPrimeConverter.connect(addr2).pledgerTKN(pledgeAmount2);
+            await rtknToPrimeConverter.connect(addr3).pledgerTKN(pledgeAmount3);
+
+            await rtknToPrimeConverter.connect(owner).startPhase2();
+            const batchSize = 2;
+            await rtknToPrimeConverter.connect(addr1).processUsers(batchSize);
+        });
+
+        it("Should allow owner to withdraw remaining rTKN after processing", async function () {
+            await rtknToPrimeConverter.connect(addr1).processUsers(1);
+            const contractBalanceBefore = await rtkn.balanceOf(rtknToPrimeConverter.address);
+            const ownerBalanceBefore = await rtkn.balanceOf(owner.address);
+
+            await rtknToPrimeConverter.connect(owner).withdrawrTKN();
+
+            const contractBalanceAfter = await rtkn.balanceOf(rtknToPrimeConverter.address);
+            const ownerBalanceAfter = await rtkn.balanceOf(owner.address);
+
+            expect(contractBalanceAfter).to.equal(0);
+            expect(ownerBalanceAfter.sub(ownerBalanceBefore)).to.equal(contractBalanceBefore);
+        });
+
+        it("Should not allow withdrawal before processing all users", async function () {
+            await expect(rtknToPrimeConverter.connect(owner).withdrawrTKN()).to.be.revertedWith("Processing not complete");
+        });
+
+        it("Should not allow non-owner to withdraw", async function () {
+            await expect(rtknToPrimeConverter.connect(addr1).withdrawrTKN()).to.be.revertedWith("Ownable: caller is not the owner");
+        });
+    });
+
+    describe("Edge Cases", function () {
+        it("Should handle no pledges gracefully", async function () {
+            await rtknToPrimeConverter.connect(owner).startPhase2();
+            const batchSize = 1;
+            await rtknToPrimeConverter.connect(owner).processUsers(batchSize);
+            expect(await rtknToPrimeConverter.getTotalUsers()).to.equal(0);
+        });
+
+        it("Should not allow starting Phase 2 multiple times", async function () {
+            await rtknToPrimeConverter.connect(owner).startPhase2();
+            await expect(rtknToPrimeConverter.connect(owner).startPhase2()).to.be.revertedWith("Already in Phase 2");
+        });
+
+        it("Should not allow pledging after Phase 2 starts", async function () {
+            await rtknToPrimeConverter.connect(owner).startPhase2();
+            const pledgeAmount = ethers.utils.parseEther("100");
+            await rtkn.connect(addr1).approve(rtknToPrimeConverter.address, pledgeAmount);
+            await expect(rtknToPrimeConverter.connect(addr1).pledgerTKN(pledgeAmount)).to.be.revertedWith("Pledging not allowed in current phase");
+        });
+    });
+});
