288e9208b1fa1f14d7e6eff059133eb4f71bb19e
kavsky02 <kamilovsky@deltaprime.io>
2024-10-29 13:46:35 +0100

    Extend check-diamond-selectors-status.js to check for other related facets/selectors

--- tools/scripts/check-diamond-selectors-status.js
+++ tools/scripts/check-diamond-selectors-status.js
@@ -10,6 +10,7 @@ const diamondAddress = {
 }
 const DiamondLoupeAbi = [
     'function facetAddress(bytes4 _functionSelector) external view returns (address)',
+    'function facetFunctionSelectors(address _facet) external view returns (bytes4[] memory facetFunctionSelectors_)',
 ]
 
 
@@ -22,39 +23,27 @@ async function main(chain = "avalanche") {
     let activeSelectors = []
     let inactiveSelectors = []
     let provider = new ethers.providers.JsonRpcProvider(jsonRPC[chain]);
+    let facetsContainingSelectors = [];
+    let allSelectorsFromRelevantFacets = new Set();
     let diamondLoupeContract = new ethers.Contract(diamondAddress[chain], DiamondLoupeAbi, provider);
     let selectorsToCheck = [
-        "0x571a6ade",
-        "0x1c0de81a",
-        "0x22f071b7",
-        "0x58d48fbc",
-        "0xa4f2e3ef",
-        "0x27123874",
-        "0x2ae387cb",
-        "0x12ae43b3",
-        "0xd5fe1426",
-        "0x76695ca9",
-        "0x4f5a254a",
-        "0xc1313fcb",
-        "0x3abaf890",
-        "0x48c2851d",
-        "0x116bb1cb",
-        "0x3436f6b8",
-        "0xfdc520c5",
-        "0x3ddfb065",
-        "0xe5cda91a",
-        "0xe42454e7",
-        "0x778c13bc",
-        "0x7f7cc4ae",
-        "0xe4a4e63b",
-        "0x828e8d8d",
-        "0x7e94ea73",
-        "0x18c9991c",
-        "0xf0094cdc",
-        "0xf4d054b4",
-        "0x974d510b",
-        "0x57126d0d",
-        "0x14604744",
+        "0xe46bbc9e",
+        "0x040cf020",
+        "0x7c93ec30",
+        "0x00f989ad",
+        "0xd66e2979",
+        "0x9d9a355e",
+        "0x3a0551a7",
+        "0x1281f5fe",
+        "0x8913e62c",
+        "0x4636afb5",
+        "0xd0c86caf",
+        "0x9df169f7",
+        "0x7c5fc3fb",
+        "0x227c74ff",
+        "0x30f4ee27",
+        "0x6bdd9e9f",
+        "0xc37c956b",
     ]
 
     if (!selectorsAreUnique(selectorsToCheck)) {
@@ -71,10 +60,26 @@ async function main(chain = "avalanche") {
             inactiveSelectors.push(selector);
         } else {
             activeSelectors.push(selector);
+            if(!facetsContainingSelectors.includes(facetAddress)) {
+                facetsContainingSelectors.push(facetAddress);
+            }
+        }
+    }
+
+    for(const facet of facetsContainingSelectors) {
+        console.log(`Additionally checking selectors for facet at address: ${facet}`);
+        let diamondLoupeContract = new ethers.Contract(diamondAddress[chain], DiamondLoupeAbi, provider);
+        let facetSelectors = await diamondLoupeContract.facetFunctionSelectors(facet);
+        for(const selector of facetSelectors) {
+            allSelectorsFromRelevantFacets.add(selector);
         }
     }
 
     console.log(`Active Selectors (${activeSelectors.length}): ${activeSelectors}`);
     console.log(`Inactive Selectors (${inactiveSelectors.length}): ${inactiveSelectors}`);
+
+    // selectors in allSelectorsFromRelevantFacets that are not in selectorsToCheck
+    let missingSelectors = Array.from(allSelectorsFromRelevantFacets).filter(x => !selectorsToCheck.includes(x));
+    console.log(`Missing Selectors (${missingSelectors.length}): ${missingSelectors}`);
 }
-main("arbitrum");
+main("avalanche");
