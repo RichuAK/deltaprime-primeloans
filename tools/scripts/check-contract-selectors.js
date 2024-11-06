571004466020d8939a76f21cc0af89c9f0c40a49
kavsky02 <kamilovsky@deltaprime.io>
2024-11-06 12:42:48 +0100

    Add check-contract-selectors

---
+++ tools/scripts/check-contract-selectors.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const { ethers } = require('ethers');
+
+async function main() {
+    // Replace 'YourContract' with the name of your contract
+    const contractSubPath = 'facets/avalanche';
+    const contractName = 'YieldYakWombatFacet';
+
+    // Construct the path to the contract artifact
+    const artifactPath = path.join(
+        __dirname,
+        `../../artifacts/contracts/${contractSubPath}/${contractName}.sol/${contractName}.json`
+    );
+
+    // Read and parse the contract artifact
+    const artifact = JSON.parse(fs.readFileSync(artifactPath, 'utf8'));
+    const abi = artifact.abi;
+
+    // Create an Interface instance from the ABI
+    const iface = new ethers.utils.Interface(abi);
+
+    // Iterate over each function in the ABI
+    for (const func of Object.values(iface.functions)) {
+        // Get the function selector
+        const selector = iface.getSighash(func);
+
+        // Log the function signature and its selector
+        console.log(`${func.format()} : ${selector}`);
+    }
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
