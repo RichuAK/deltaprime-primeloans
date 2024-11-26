5ead2473f409b6d14fe997e1c7dcbc6dec48f4ac
richuak <richuakuttikattu@gmail.com>
2024-11-26 15:33:36 +0530

    add all swap structs, decode data: multiswap fails with stack too deep

--- contracts/facets/ParaSwapFacet.sol
+++ contracts/facets/ParaSwapFacet.sol
@@ -24,6 +24,12 @@ contract ParaSwapFacet is ReentrancyGuardKeccak, SolvencyMethods {
     address private constant PARA_ROUTER =
         0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57;
 
+    
+    ///@notice selectors for paraSwapV2 data decoding
+    bytes4 private constant DIRECT_UNI_V3_SELECTOR = 0xa6886da9;
+    bytes4 private constant SIMPLESWAP_SELECTOR = 0x54e3f31b;
+    bytes4 private constant MULTISWAP_SELECTOR = 0xa94e78ef;
+    
     struct SwapTokensDetails {
         bytes32 tokenSoldSymbol;
         bytes32 tokenBoughtSymbol;
@@ -33,27 +39,118 @@ contract ParaSwapFacet is ReentrancyGuardKeccak, SolvencyMethods {
         uint256 initialBoughtTokenBalance;
     }
 
-    struct DecodedData {
-        address fromTokenData;
-        address toTokenData;
-        address exchangeData;
-        uint256 fromAmountData;
-        uint256 toAmountData;
-        uint256 expectedAmountData;
-        uint256 feePercentData;
-        uint256 deadlineData;
-        address partnerData;
-        bool isApprovedData;
-        address beneficiaryData;
-        bytes pathData;
-        bytes permitData;
-        bytes16 uuidData;
+    
+    ///@notice struct for directUniV3Swap method for ParaSwapRouter, derived manually from real transaction data onchain.
+    ///@dev successfully decodes data in test. Selector is 0xa6886da9
+    struct DirectUniswapV3SwapData {
+        address fromToken;
+        address toToken;
+        address exchange;
+        uint256 fromAmount;
+        uint256 toAmount;
+        uint256 expectedAmount;
+        uint256 feePercent;
+        uint256 deadline;
+        address partner;
+        bool isApproved;
+        address beneficiary;
+        bytes path;
+        bytes permit;
+        bytes16 uuid;
     }
 
-    // (address fromToken, address toToken, address exchange, uint256 fromAmount, uint256 toAmount, 
-    // uint256 expectedAmount, uint256 feePercent, uint256 deadline, 
-    // address partner, bool isApproved, address beneficiary, bytes path, bytes permit, bytes16 uuid)
+    /// @notice simpleSwap data Struct from ParaSwapRouter, manually derived from real transaction data onchain.
+    /// @dev test passes, data gets decoded. Selector is 0x54e3f31b
+    struct SimpleSwapData{
+        address fromToken; 
+        address toToken; 
+        uint256 fromAmount; 
+        uint256 toAmount; 
+        uint256 expectedAmount; 
+        address[] callees; 
+        bytes exchangeData;
+        uint256[] startIndexes; 
+        uint256[] values; 
+        address beneficiary; 
+        address partner; 
+        uint256 feePercent; 
+        bytes permit; 
+        uint256 deadline; 
+        bytes16 uuid;
+    }
 
+    ///@notice multiSwap data Struct from ParaSwap Github
+    /// https://github.com/paraswap/augustus-v5/blob/d297477b8fc7be65c337b0cf2bc21f4f7f925b68/contracts/routers/MultiPath.sol#L43
+    /// https://github.com/paraswap/augustus-v5/blob/d297477b8fc7be65c337b0cf2bc21f4f7f925b68/contracts/lib/Utils.sol#L61
+    /// @dev test fails with stack too deep error. Selector is 0xa94e78ef
+    //////******* MultiSwap Structs Begin *******//////// 
+    /**
+     * @param fromToken Address of the source token
+     * @param fromAmount Amount of source tokens to be swapped
+     * @param toAmount Minimum destination token amount expected out of this swap
+     * @param expectedAmount Expected amount of destination tokens without slippage
+     * @param beneficiary Beneficiary address
+     * 0 then 100% will be transferred to beneficiary. Pass 10000 for 100%
+     * @param path Route to be taken for this swap to take place
+     */
+    struct SellData {
+        address fromToken;
+        uint256 fromAmount;
+        uint256 toAmount;
+        uint256 expectedAmount;
+        address payable beneficiary;
+        Path[] path;
+        address payable partner;
+        uint256 feePercent;
+        bytes permit;
+        uint256 deadline;
+        bytes16 uuid;
+    }
+
+    struct Path {
+        address to;
+        uint256 totalNetworkFee; //NOT USED - Network fee is associated with 0xv3 trades
+        Adapter[] adapters;
+    }
+
+    struct Adapter {
+        address payable adapter;
+        uint256 percent;
+        uint256 networkFee; //NOT USED
+        Route[] route;
+    }
+
+    struct Route {
+        uint256 index; //Adapter at which index needs to be used
+        address targetExchange;
+        uint256 percent;
+        bytes payload;
+        uint256 networkFee; //NOT USED - Network fee is associated with 0xv3 trades
+    }
+
+    //////******* MultiSwap Struct End *******//////// 
+
+    /// SellData cut in half to try and avoid stack too deep///
+    struct SellDataOne{
+        address fromToken;
+        uint256 fromAmount;
+        uint256 toAmount;
+        uint256 expectedAmount;
+        address payable beneficiary;
+        Path[] path;
+    }
+
+    struct SellDataTwo{
+        address payable partner;
+        uint256 feePercent;
+        bytes permit;
+        uint256 deadline;
+        bytes16 uuid;
+    }
+
+
+    
+    
     function getInitialTokensDetails(
         address _soldTokenAddress,
         address _boughtTokenAddress
@@ -158,7 +255,7 @@ contract ParaSwapFacet is ReentrancyGuardKeccak, SolvencyMethods {
 
     function paraSwapV2(
         bytes4 selector,
-        bytes memory data,  ///@dev changing data from memory to calldata for slicing
+        bytes calldata data,  ///@dev changing data from memory to calldata for slicing
         address fromToken,
         uint256 fromAmount,
         address toToken,
@@ -179,12 +276,12 @@ contract ParaSwapFacet is ReentrancyGuardKeccak, SolvencyMethods {
         require(minOut > 0, "minOut needs to be > 0");
         require(fromAmount > 0, "Amount of tokens to sell has to be greater than 0");
 
+        console.log("Inside ParaSwap v2");
+        bytes memory selectorBytes = abi.encodePacked(selector);
+        console.log("Selector: ");
+        console.logBytes(selectorBytes);
         /// @dev would need to slice the data before decoding, I think
         // {
-        //     console.log("Inside ParaSwap v2");
-        //     bytes memory selectorBytes = abi.encodePacked(selector);
-        //     console.log("Selector: ");
-        //     console.logBytes(selectorBytes);
 
         //     bytes memory firstThreeAddresses = data[:96];
         //     address fromTokenFromData;
@@ -202,55 +299,33 @@ contract ParaSwapFacet is ReentrancyGuardKeccak, SolvencyMethods {
         //     console.log(exchangeFromData);
         // }
 
-        ///@dev special scoping here to avoid stack too deep error
+        ///@dev special scoping to avoid stack too deep error. Update: scoping inside if else statements, still valid
         /// https://stackoverflow.com/questions/74578910/how-to-fix-compilererror-stack-too-deep-try-compiling-with-via-ir-cli
         /// UniSwap does the same apparently: https://ethereum.stackexchange.com/questions/6061/error-while-compiling-stack-too-deep
-        {
-            DecodedData memory decodedData = abi.decode(data, (DecodedData));
-            console.log("From Token:");
-            console.log(decodedData.fromTokenData);
-            console.log("To Token:");
-            console.log(decodedData.toTokenData);
-            console.log("Exchange:");
-            console.log(decodedData.exchangeData);
-            console.log("From Amount:");
-            console.log(decodedData.fromAmountData);
-            console.log("To Amount:");
-            console.log(decodedData.toAmountData);
-            console.log("Expected Amount:");
-            console.log(decodedData.expectedAmountData);
-            console.log("Fee Percent:");
-            console.log(decodedData.feePercentData);
-            console.log("Deadline:");
-            console.log(decodedData.deadlineData);
-            console.log("Partner:");
-            console.log(decodedData.partnerData);
-            console.log("Is Approved:");
-            console.log(decodedData.isApprovedData);
-            // console.log("Beneficiary:");
-            // console.log(decodedData.beneficiaryData);
-            // console.log("Path:");
-            // console.logBytes(decodedData.pathData);
-            // console.log("Permit:");
-            // console.logBytes(decodedData.permitData);
-            // bytes memory uuidBytes = abi.encodePacked(decodedData.uuidData);
-            // console.log("UUID:");
-            // console.logBytes(uuidBytes);
-
+        
+        if(selector == DIRECT_UNI_V3_SELECTOR){
+            _decodeDirectUniV3SwapData(data);
+        } else if(selector == SIMPLESWAP_SELECTOR){
+            _decodeSimpleSwapData(data);
+        } else if(selector == MULTISWAP_SELECTOR){
+            // _decodeMultiSwapData(data);
+            console.log("MultiSwap PlaceHolder");
+        } else {
+            console.log("Not My Selector!");
         }
 
-        {
-            DecodedData memory decodedData = abi.decode(data, (DecodedData));
-            console.log("Beneficiary:");
-            console.log(decodedData.beneficiaryData);
-            console.log("Path:");
-            console.logBytes(decodedData.pathData);
-            console.log("Permit:");
-            console.logBytes(decodedData.permitData);
-            bytes memory uuidBytes = abi.encodePacked(decodedData.uuidData);
-            console.log("UUID:");
-            console.logBytes(uuidBytes);
-        }
+        // {
+        //     DecodedData memory decodedData = abi.decode(data, (DecodedData));
+        //     console.log("Beneficiary:");
+        //     console.log(decodedData.beneficiaryData);
+        //     console.log("Path:");
+        //     console.logBytes(decodedData.pathData);
+        //     console.log("Permit:");
+        //     console.logBytes(decodedData.permitData);
+        //     bytes memory uuidBytes = abi.encodePacked(decodedData.uuidData);
+        //     console.log("UUID:");
+        //     console.logBytes(uuidBytes);
+        // }
         address(swapTokensDetails.soldToken).safeApprove(PARA_TRANSFER_PROXY, 0);
         address(swapTokensDetails.soldToken).safeApprove(
             PARA_TRANSFER_PROXY,
@@ -284,6 +359,100 @@ contract ParaSwapFacet is ReentrancyGuardKeccak, SolvencyMethods {
             boughtTokenFinalAmount,
             block.timestamp
         );
+    }
+
+    function _decodeDirectUniV3SwapData(bytes memory _data) internal pure{
+        DirectUniswapV3SwapData memory decodedData = abi.decode(_data, (DirectUniswapV3SwapData));
+            console.log("Decoding directUniV3Swap Data");
+            console.log("From Token:");
+            console.log(decodedData.fromToken);
+            console.log("To Token:");
+            console.log(decodedData.toToken);
+            console.log("Exchange:");
+            console.log(decodedData.exchange);
+            console.log("From Amount:");
+            console.log(decodedData.fromAmount);
+            console.log("To Amount:");
+            console.log(decodedData.toAmount);
+            console.log("Expected Amount:");
+            console.log(decodedData.expectedAmount);
+            console.log("Fee Percent:");
+            console.log(decodedData.feePercent);
+            console.log("Deadline:");
+            console.log(decodedData.deadline);
+            console.log("Partner:");
+            console.log(decodedData.partner);
+            console.log("Is Approved:");
+            console.log(decodedData.isApproved);
+            // console.log("Beneficiary:");
+            // console.log(decodedData.beneficiaryData);
+            // console.log("Path:");
+            // console.logBytes(decodedData.pathData);
+            // console.log("Permit:");
+            // console.logBytes(decodedData.permitData);
+            // bytes memory uuidBytes = abi.encodePacked(decodedData.uuidData);
+            // console.log("UUID:");
+            // console.logBytes(uuidBytes);
+
+    }
+
+    function _decodeSimpleSwapData(bytes memory _data) internal pure{
+        SimpleSwapData memory decodedData = abi.decode(_data, (SimpleSwapData));
+            console.log("Decoding SimpleSwap Data");
+            console.log("From Token:");
+            console.log(decodedData.fromToken);
+            console.log("To Token:");
+            console.log(decodedData.toToken);
+            // console.log("Exchange:");
+            // console.log(decodedData.exchangeData);
+            console.log("From Amount:");
+            console.log(decodedData.fromAmount);
+            console.log("To Amount:");
+            console.log(decodedData.toAmount);
+            console.log("Expected Amount:");
+            console.log(decodedData.expectedAmount);
+            console.log("Exchange Data:");
+            bytes memory exchangeDataBytes = abi.encodePacked(decodedData.exchangeData);
+            console.logBytes(exchangeDataBytes);
+            console.log("Fee Percent:");
+            console.log(decodedData.feePercent);
+            console.log("Deadline:");
+            console.log(decodedData.deadline);
+            console.log("Partner:");
+            console.log(decodedData.partner);
+            console.log("Beneficiary:");
+            console.log(decodedData.beneficiary);
+    }
+
+    function _decodeMultiSwapData(bytes memory _data) internal pure {
+            // SellData memory decodedData = abi.decode(_data, (SellData));
+            SellDataOne memory decodedDataOne;
+            SellDataTwo memory decodedDataTwo;
+            (decodedDataOne,decodedDataTwo) = abi.decode(_data, (SellDataOne,SellDataTwo));
+            console.log("Decoding MultiSwap Data");
+            console.log("From Token:");
+            console.log(decodedDataOne.fromToken);
+            console.log("From Amount:");
+            console.log(decodedDataOne.fromAmount);
+            console.log("To Amount:");
+            console.log(decodedDataOne.toAmount);
+            console.log("Expected Amount:");
+            console.log(decodedDataOne.expectedAmount);
+            console.log("Beneficiary:");
+            console.log(decodedDataOne.beneficiary);
+            console.log("Fee Percent:");
+            console.log(decodedDataTwo.feePercent);
+            console.log("Deadline:");
+            console.log(decodedDataTwo.deadline);
+            console.log("Partner:");
+            console.log(decodedDataTwo.partner);
+            // console.log("Path:");
+            // console.logBytes(decodedData.pathData);
+            console.log("Permit:");
+            console.logBytes(decodedDataTwo.permit);
+            bytes memory uuidBytes = abi.encodePacked(decodedDataTwo.uuid);
+            console.log("UUID:");
+            console.logBytes(uuidBytes);
     }
 
     modifier onlyOwner() {
