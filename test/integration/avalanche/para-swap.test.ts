0f869c2c48e8f36a177c03baf594668cbd865ca0
richuak <richuakuttikattu@gmail.com>
2024-11-27 14:38:22 +0530

    edit getSwapData() to only give multiSwap data for testing

--- test/integration/avalanche/para-swap.test.ts
+++ test/integration/avalanche/para-swap.test.ts
@@ -87,6 +87,11 @@ describe("ParaSwap", () => {
         amount: srcAmount.toString(),
         userAddress: wrappedLoan.address,
         side: SwapSide.SELL,
+        /// forcing API to always give a multiSwap route. For testing multiSwap swapData dea
+        // https://github.com/paraswap/paraswap-sdk/blob/17c2c2162fac8a5cb18aaa9588c44c0c6545f8f7/src/methods/swap/rates.ts#L74
+        // excludeContractMethods: ["simpleSwap", "directUniV3Swap"],
+        includeContractMethods: "multiSwap",
+        // excludeDEXS: "UniswapV3",
       });
       const txParams = await paraSwapMin.swap.buildTx(
         {
